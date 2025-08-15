d89477f06efbdcaf3c8277c49d1fe5c48a9a3df9
roshkhan2000 <Roshaank2000@gmail.com>
2025-08-15 16:39:42 +0100

    Time: 502 ms (48.73%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+SELECT
+    s.user_id,
+    ROUND(
+        CAST(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) AS FLOAT) 
+        / COUNT(*), 
+        2
+    ) AS confirmation_rate
+FROM
+    Signups s
+LEFT JOIN
+    Confirmations c ON s.user_id = c.user_id
+GROUP BY
+    s.user_id;
