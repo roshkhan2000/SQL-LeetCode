f84f865914ca921728d84e83bc16bf87e8d4580a
roshkhan2000 <Roshaank2000@gmail.com>
2025-08-16 14:48:30 +0100

    Time: 276 ms (46.6%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM
+    cinema 
+WHERE
+    id % 2 != 0
+    AND
+    description != 'boring'
+ORDER BY
+    rating DESC
