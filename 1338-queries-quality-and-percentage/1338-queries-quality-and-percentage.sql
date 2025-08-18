765661180c8e165872dfff92910ae17c988646c9
roshkhan2000 <Roshaank2000@gmail.com>
2025-08-18 11:49:32 +0100

    Time: 981 ms (58.76%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -2,15 +2,17 @@
 SELECT
     query_name,
     ROUND(AVG(CAST(rating AS DECIMAL) / CAST(position AS DECIMAL)), 2) AS quality, 
-    ROUND(
-            (
-            CAST(SUM(CASE rating WHEN 0 THEN 1 WHEN 1 THEN 1 WHEN 2 THEN 1 ELSE 0 END) AS NUMERIC) 
-            / 
-            CAST(COUNT(query_name) AS NUMERIC)
-            *
-            100
-            ) 
-    , 2) AS poor_query_percentage
+    ROUND
+        (
+                (
+                CAST(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) AS NUMERIC) 
+                / 
+                CAST(COUNT(query_name) AS NUMERIC)
+                *
+                100
+                ) 
+        , 2
+    ) AS poor_query_percentage
 FROM
     queries
 GROUP BY
